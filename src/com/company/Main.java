09f7aff117759cc85832a30acfa2f3f17d01327d
GrucaEugene <evheniihrutsa@gmail.com>
2021-11-08 22:50:35 +0200

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,8 +15,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(getReversed("I go to school"));
-
+        System.out.println(getReversed("I went to school"));
     }
 }
 

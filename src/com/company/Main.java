69ebd57e35460d8fc47f8d7007948a1ebc11eba3
GrucaEugene <evheniihrutsa@gmail.com>
2021-11-08 22:50:35 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static String getReversed(String s) {
+        List<String> words = Arrays.asList(s.split(" "));
+        Collections.reverse(words);
+
+        return words.stream()
+                .reduce((accum, item) -> accum.concat(" " + item)).orElseThrow();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getReversed("I go to school"));
+
+    }
+}
+
